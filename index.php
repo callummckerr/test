950bc937e13c3a2385b9496cb18b7b802e54fb2f
callummckerr <callum@mckerrs.net>
2015-04-24 10:13:40 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+$profpic = benteke.jpg
 
 echo "<h2>christian benteke appreciation club</h2>";
-echo "2-1<br>";
+echo "avl 2-1 lvp<br>";
 echo "a top quality player - tactics tim";
-
 ?>
-<script>
-  window.alert("CHEEKI BREEKI");
-</script>
+
+<style type="text/css" id="custom-background-css">
+body.custom-background { background-image: url('C:\Users\mckerrcb\Desktop\fresh memes 2.0\benteke.jpg'); background-repeat: no-repeat; background-position: center ; background-attachment: fixed; }
+</style>
